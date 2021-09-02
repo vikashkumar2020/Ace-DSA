3596ad35dee0298af283755e72a3b4c38a4f8c60
kvvik2020 <kvvik2020@gmail.com>
2021-09-03 02:44:20 +0530

    array

---
+++ Array/Duplicates_in_array.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ int duplicates(vector<int>& nums) {
+        int curr_1 = nums[0];
+        int curr_2 = nums[0];
+        while (1) {
+            curr_1 = nums[curr_1];
+            curr_2 = nums[nums[curr_2]];
+            if (curr_1 == curr_2) break;
+        }
+        curr_1 = nums[0];
+        while (curr_1!=curr_2) {
+            curr_1 = nums[curr_1];
+            curr_2 = nums[curr_2];
+        }
+        return curr_2;
+    
+    }
+
+    int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        vector<int>a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+      int ans = duplicates(a);
+        cout<<ans;
+        cout << endl;
+    }
+    return 0;
+}
